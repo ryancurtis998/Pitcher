0d82643e9f94d360bdcca48b9d35eb01383ddcf1
santa <ryancurtis998@yahoo.com>
2019-04-23 11:07:31 +0300

    updated

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY='ryan'
-export MAIL_USERNAME='moringaprojects395@gmail.com'
-export MAIL_PASSWORD='moringa123'
+export MAIL_USERNAME='pitch448@gmail.com'
+export MAIL_PASSWORD='62211327cur'
 
 python3.6 manage.py server
