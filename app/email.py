30a07f145b7e7e5369b86845aeda4df461ad4688
santa <ryancurtis998@yahoo.com>
2019-04-23 11:00:31 +0300

    initialising migrations

--- app/email.py
+++ app/email.py
@@ -8,4 +8,4 @@ def mail_message(subject,template,to,**kwargs):
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
     email.txt = render_template(template + ".txt",**kwargs)
-mail.send(email)
+    mail.send(email)
---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY='ryan'
+export MAIL_USERNAME='moringaprojects395@gmail.com'
+export MAIL_PASSWORD='moringa123'
+
+python3.6 manage.py server
